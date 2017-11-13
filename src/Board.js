c519b51841fdbb6a82677f3c3bc07d7cfb4a5a69
irbekrm <irbekrm@gmail.com>
2017-11-13 17:00:29 +0100

    click handlers

--- src/Board.js
+++ src/Board.js
@@ -10,10 +10,8 @@ class Board extends Component{
   };
 
   componentDidMount(){
-    this.mounted = true;
     this.generate();
     this.setTimer(1000);
-    this.generation = 0;
   }
 
   componentDidUpdate() {
@@ -21,14 +19,16 @@ class Board extends Component{
     this.generate();
   }
 
-  setTimer = interval => {this.timer = window.setInterval(this.checkCells, interval); };
-  removeTimer = _ => {window.clearInterval(this.timer)};
+  setTimer = interval => this.timer = window.setInterval(this.checkCells, interval);
+  removeTimer = _ => window.clearInterval(this.timer);
 
   removeCells = _ => {
     select("#wrapper")
       .selectAll("div")
         .remove()
   }
+
+
   generate = _ => {
     let self = this;
 
@@ -38,9 +38,15 @@ class Board extends Component{
       .enter().append("div")
       .style("background-color",
         d => d == 1 ? "rgb(51, 153, 255)" : d == 2 ? "rgb(0, 64, 128)" : "rgb(0,26,0)")
+      .on("click", (d,i) =>  self.clickHandler(d,i))
     };
 
   checkIfWithinRange = x => x >= 0 && x < 4000;
+
+  clickHandler = (d,i) => {
+    let x = d < 2 ? d + 1 : 0;
+    this.setState(prevState => ({arr: (prevState.arr[i] = x, prevState.arr)}))
+  }
 
   getNeighbours = i => {
     let arr = [];
@@ -49,6 +55,7 @@ class Board extends Component{
     });
     return arr;
   };
+
   countLivesAround = (surroundingCells, arr) =>
     surroundingCells.reduce((a,b) => a + (arr[b] ? 1 : 0),0);
 
@@ -62,7 +69,7 @@ class Board extends Component{
 
   checkCells = _ => {
     let oldCells = this.state.arr.slice(0);
-    let liveCells = oldCells.map((e,i) => [e,i]).filter(x=>x[0]==1).map(y=>y[1]);
+    let liveCells = oldCells.map((e,i) => [e,i]).filter(x=>!!x[0]).map(y=>y[1]);
     let newCells = liveCells.concat(...liveCells.map(i => this.getNeighbours(i)));
 
     let newArr = oldCells.map((e,i) => newCells.includes(i) ?
